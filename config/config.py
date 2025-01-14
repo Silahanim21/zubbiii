b477090da650a1162bafb2b8fead4d221dece455
Silahanim21 <turkcell63632@gmail.com>
2025-01-15 00:23:04 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -132,7 +132,7 @@ TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "5"))
 
 # Your Github Repo.. Will be shown on /start Command
 
-GITHUB_REPO = getenv("GITHUB_REPO", None) 
+GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/kumsalfed6301/Kumsal-muzik-bot") 
 
 # Spotify Client.. Get it from https://developer.spotify.com/dashboard
 
